494a0f9c7c2e3f5a1f701dc8024b938a84cf81bc
Unagui19 <miniveliz14@gmail.com>
2023-06-15 19:13:17 -0300

    Archivos para el RPG

---
+++ Characteristics.cs
@@ -0,0 +1,17 @@
+namespace RPG;
+
+public class Characteristics_Character{
+    private int velocity;
+    private int force;
+    private int level;
+    private int armor;
+    private int skill;
+    private int health;
+
+    public int Velocity { get => velocity; set => velocity = value; }
+    public int Force { get => force; set => force = value; }
+    public int Level { get => level; set => level = value; }
+    public int Armor { get => armor; set => armor = value; }
+    public int Skill { get => skill; set => skill = value; }
+    public int Health { get => health; set => health = value; }
+}
