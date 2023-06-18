a5b6517582dd07c2ce3e3eb02646535c0ca60731
adanSmith01 <smith.paredes.13@gmail.com>
2023-06-18 20:25:36 -0300

    Se muestran los datos y caracteristicas de los personajes

--- Program.cs
+++ Program.cs
@@ -5,13 +5,36 @@ internal class Program
     private static void Main(string[] args)
     {
         string path = "rpgPersonajes.json";
+        List<Personaje> listaPersonajes = new List<Personaje>();
 
         if(!PersonajesJSON.ExisteArchivoPersonajes(path)){
             PersonajesJSON.GuardarPersonajes(FabricaDePersonajes.GenerarListaPersonajesRPG(), path);
-            List<Personaje> listaPersonajes = PersonajesJSON.LeerPersonajes(path);
+            listaPersonajes = PersonajesJSON.LeerPersonajes(path);
         } else{
-            List<Personaje> listaPersonajes = PersonajesJSON.LeerPersonajes(path);
+            listaPersonajes = PersonajesJSON.LeerPersonajes(path);
         }
+
+        // Se muestran los datos y características de los personajes
+        mostrarPersonajes(listaPersonajes);
         
+    }
+
+    public static void mostrarPersonajes(List<Personaje> personajes){
+        foreach (var p in personajes){
+            Console.WriteLine($"┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ PERSONAJE: {p.Nombre.ToUpper()} ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐");
+            Console.WriteLine($"| ===========DATOS===========");
+            Console.WriteLine($"| NOMBRE: {p.Nombre}");
+            Console.WriteLine($"| APODO: {p.Apodo}");
+            Console.WriteLine($"| TIPO DE PERSONAJE: {p.Tipo.ToString()}");
+            Console.WriteLine($"| FECHA DE NACIMIENTO: {p.FechaNac.ToShortDateString()}");
+            Console.WriteLine($"| EDAD: {p.Edad}");
+            Console.WriteLine($"| ===========CARACTERÍSTICAS===========");
+            Console.WriteLine($"| VELOCIDAD: {p.DatosSecundarios.Velocidad}");
+            Console.WriteLine($"| DESTREZA: {p.DatosSecundarios.Destreza}");
+            Console.WriteLine($"| FUERZA: {p.DatosSecundarios.Fuerza}");
+            Console.WriteLine($"| NIVEL: {p.DatosSecundarios.Nivel}");
+            Console.WriteLine($"| ARMADURA: {p.DatosSecundarios.Armadura}");
+            Console.WriteLine($"| SALUD: {p.DatosSecundarios.Salud}");
+        }
     }
 }
