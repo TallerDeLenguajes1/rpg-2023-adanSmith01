fa9fddb7f528680e311d9f19ffcae653674feb19
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-18 23:38:39 -0300

    Caracteristicas del personaje

--- CaracteristicasPersonaje.cs
+++ CaracteristicasPersonaje.cs
@@ -1,18 +1,18 @@
 namespace RPG;
 
-public class Caracteristicas
+public class CaracteristicasPersonaje
 {
     private int velocidad;
     private int destreza;
     private int fuerza;
     private int nivel;
-    private int armadura;
+    private int blindaje;
     private float salud;
 
     public int Velocidad { get => velocidad; set => velocidad = value; }
     public int Destreza { get => destreza; set => destreza = value; }
     public int Fuerza { get => fuerza; set => fuerza = value; }
     public int Nivel { get => nivel; set => nivel = value; }
-    public int Armadura { get => armadura; set => armadura = value; }
+    public int Blindaje { get => blindaje; set => blindaje = value; }
     public float Salud { get => salud; set => salud = value; }
 }
