cffb8ed6ce7428cd8cc531d1de441b38c6c2f955
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-18 23:41:01 -0300

    Clase que define al personaje del juego

--- Personaje.cs
+++ Personaje.cs
@@ -1,12 +1,24 @@
 namespace RPG;
 
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
 // Los tipos de personajes que aparecerán en el juego
 public enum TipoPersonaje
 {
+    [EnumMember(Value = "Sicario")]
     Sicario,
-    Prisionero,
+
+    [EnumMember(Value = "Psicopata")]
+    Psicopata,
+
+    [EnumMember(Value = "Ladron")]
     Ladron,
+
+    [EnumMember(Value = "Policia")]
     Policia,
+
+    [EnumMember(Value = "AgenteEspecial")]
     AgenteEspecial
 }
 
@@ -16,13 +28,18 @@ public class Personaje
     private string apodo;
     private TipoPersonaje tipo;
     private DateTime fechaNac;
+    private string nacionalidad;
     private int edad;
-    private Caracteristicas datosSecundarios = new Caracteristicas();
+    private CaracteristicasPersonaje caracteristicas;
 
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apodo { get => apodo; set => apodo = value; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public TipoPersonaje Tipo { get => tipo; set => tipo = value; }
     public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
     public int Edad { get => edad; set => edad = value; }
-    public Caracteristicas DatosSecundarios { get => datosSecundarios; set => datosSecundarios = value; }
+    public string Nacionalidad { get => nacionalidad; set => nacionalidad = value;}
+    public CaracteristicasPersonaje Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
+
 }
