1c194026ca635480c49a6eea18e8148c5b2e118b
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-22 01:05:28 -0300

    Clase que define a los jugadores del juego

--- Jugadores.cs
+++ Jugadores.cs
@@ -1,8 +1,14 @@
 namespace RPG;
 
-public class Jugadores
+public class Jugador
 {
-    public string nombreJugador;
-    public Personaje P;
-    public int puntaje;
+    public string NombreJugador;
+    public Personaje Luchador;
+    private float puntaje;
+
+    public float Puntaje {get => puntaje; }
+
+    public void CalcularPuntaje(){
+        puntaje += 100 + new Random().Next(1, 100);
+    }
 }
