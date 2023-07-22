bb3923e74ddc80769d9d0fd555e62e8ad307d89c
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-22 01:02:56 -0300

    Actualización de los métodos de la clase

--- FabricaDePersonajes.cs
+++ FabricaDePersonajes.cs
@@ -5,13 +5,57 @@ namespace RPG;
 
 public class FabricaDePersonajes
 {
-    private PersonajePaises ObtenerPosiblesPaisesPersonaje(string nombrePersonaje){
+    private static Dictionary<string, string> paises = new Dictionary<string, string>()
+    { 
+        {"AD","Andorra"}, {"AE","Emiratos Árabes Unidos"}, {"AF","Afganistán"}, {"AG","Antigua y Barbuda"}, {"AI","Anguila"},
+        {"AL","Albania"}, {"AM","Armenia"}, {"AN","Antillas Neerlandesas"}, {"AO","Angola"}, {"AQ","Antártida"}, {"AR","Argentina"},
+        {"AS","Samoa Americana"}, {"AT","Austria"}, {"AU","Australia"}, {"AW","Aruba"}, {"AX","Islas Áland"}, {"AZ","Azerbaiyán"},
+        {"BA","Bosnia y Herzegovina"}, {"BB","Barbados"}, {"BD","Bangladesh"}, {"BE","Bélgica"}, {"BF","Burkina Faso"},
+        {"BG","Bulgaria"}, {"BH","Bahréin"}, {"BI","Burundi"}, {"BJ","Benin"}, {"BL","San Bartolomé"}, {"BM","Bermudas"},
+        {"BN","Brunéi"}, {"BO","Bolivia"}, {"BR","Brasil"}, {"BS","Bahamas"}, {"BT","Bhután"}, {"BV","Isla Bouvet"},
+        {"BW","Botsuana"}, {"BY","Belarús"}, {"BZ","Bélice"}, {"CA","Canadá"}, {"CC","Islas Cocos"}, {"CF","República Centro-Africana"},
+        {"CG","Congo"}, {"CH","Suiza"}, {"CI","Costa de Marfil"}, {"CK","Islas Cook"}, {"CL","Chile"}, {"CM","Camerún"}, {"CN","China"},
+        {"CO","Colombia"}, {"CR","Costa Rica"}, {"CU","Cuba"}, {"CV","Cabo Verde"}, {"CX","Islas Christmas"}, {"CY","Chipre"}, {"CZ","República Checa"},
+        {"DE","Alemania"}, {"DJ","Yibuti"}, {"DK","Dinamarca"}, {"DM","Domínica"}, {"DO","República Dominicana"}, {"DZ","Argel"},
+        {"EC","Ecuador"}, {"EE","Estonia"}, {"EG","Egipto"}, {"EH","Sahara Occidental"}, {"ER","Eritrea"}, {"ES","España"},
+        {"ET","Etiopía"}, {"FI","Finlandia"}, {"FJ","Fiji"}, {"FK","Islas Malvinas"}, {"FM","Micronesia"}, {"FO","Islas Faroe"},
+        {"FR","Francia"}, {"GA","Gabón"}, {"GB","Reino Unido"}, {"GD","Granada"}, {"GE","Georgia"}, {"GF","Guayana Francesa"},
+        {"GG","Guernsey"}, {"GH","Ghana"}, {"GI","Gibraltar"}, {"GL","Groenlandia"}, {"GM","Gambia"}, {"GN","Guinea"}, {"GP","Guadalupe"},
+        {"GQ","Guinea Ecuatorial"}, {"GR","Grecia"}, {"GS","Georgia del Sur e Islas Sandwich del Sur"}, {"GT","Guatemala"}, {"GU","Guam"},
+        {"GW","Guinea-Bissau"}, {"GY","Guayana"}, {"HK","Hong Kong"}, {"HM","Islas Heard y McDonald"}, {"HN","Honduras"}, {"HR","Croacia"},
+        {"HT","Haití"}, {"HU","Hungría"}, {"ID","Indonesia"}, {"IE","Irlanda"}, {"IL","Israel"}, {"IM","Isla de Man"}, {"IN","India"},
+        {"IO","Territorio Británico del Océano Índico"}, {"IQ","Irak"}, {"IR","Irán"}, {"IS","Islandia"}, {"IT","Italia"}, {"JE","Jersey"},
+        {"JM","Jamaica"}, {"JO","Jordania"}, {"JP","Japón"}, {"KE","Kenia"}, {"KG","Kirguistán"}, {"KH","Camboya"}, {"KI","Kiribati"},
+        {"KM","Comoros"}, {"KN","San Cristóbal y Nieves"}, {"KP","Corea del Norte"}, {"KR","Corea del Sur"}, {"KW","Kuwait"}, {"KY","Islas Caimán"},
+        {"KZ","Kazajstán"}, {"LA","Laos"}, {"LB","Líbano"}, {"LC","Santa Lucía"}, {"LI","Liechtenstein"}, {"LK","Sri Lanka"}, {"LR","Liberia"},
+        {"LS","Lesotho"}, {"LT","Lituania"}, {"LU","Luxemburgo"}, {"LV","Letonia"}, {"LY","Libia"}, {"MA","Marruecos"}, {"MC","Mónaco"},
+        {"MD","Moldova"}, {"ME","Montenegro"}, {"MG","Madagascar"}, {"MH","Islas Marshall"}, {"MK","Macedonia"}, {"ML","Mali"}, {"MM","Myanmar"},
+        {"MN","Mongolia"}, {"MO","Macao"}, {"MQ","Martinica"}, {"MR","Mauritania"}, {"MS","Montserrat"}, {"MT","Malta"}, {"MU","Mauricio"},
+        {"MV","Maldivas"}, {"MW","Malawi"}, {"MX","México"}, {"MY","Malasia"}, {"MZ","Mozambique"}, {"NA","Namibia"}, {"NC","Nueva Caledonia"},
+        {"NE","Níger"}, {"NF","Islas Norkfolk"}, {"NG","Nigeria"}, {"NI","Nicaragua"}, {"NL","Países Bajos"}, {"NO","Noruega"}, {"NP","Nepal"},
+        {"NR","Nauru"}, {"NU","Niue"}, {"NZ","Nueva Zelanda"}, {"OM","Omán"}, {"PA","Panamá"}, {"PE","Perú"}, {"PF","Polinesia Francesa"},
+        {"PG","Papúa Nueva Guinea"}, {"PH","Filipinas"}, {"PK","Pakistán"}, {"PL","Polonia"}, {"PM","San Pedro y Miquelón"}, {"PN","Islas Pitcairn"},
+        {"PR","Puerto Rico"}, {"PS","Palestina"}, {"PT","Portugal"}, {"PW","Islas Palaos"}, {"PY","Paraguay"}, {"QA","Qatar"}, {"RE","Reunión"},
+        {"RO","Rumanía"}, {"RS","Serbia y Montenegro"}, {"RU","Rusia"}, {"RW","Ruanda"}, {"SA","Arabia Saudita"}, {"SB","Islas Solomón"},
+        {"SC","Seychelles"}, {"SD","Sudán"}, {"SE","Suecia"}, {"SG","Singapur"}, {"SH","Santa Elena"}, {"SI","Eslovenia"}, {"SJ","Islas Svalbard y Jan Mayen"},
+        {"SK","Eslovaquia"}, {"SL","Sierra Leona"}, {"SM","San Marino"}, {"SN","Senegal"}, {"SO","Somalia"}, {"SR","Surinam"}, {"ST","Santo Tomé y Príncipe"},
+        {"SV","El Salvador"}, {"SY","Siria"}, {"SZ","Suazilandia"}, {"TC","Islas Turcas y Caicos"}, {"TD","Chad"}, {"TF","Territorios Australes Franceses"},
+        {"TG","Togo"}, {"TH","Tailandia"}, {"TZ","Tanzania"}, {"TJ","Tayikistán"}, {"TK","Tokelau"}, {"TL","Timor-Leste"}, {"TM","Turkmenistán"},
+        {"TN","Túnez"}, {"TO","Tonga"}, {"TR","Turquía"}, {"TT","Trinidad y Tobago"}, {"TV","Tuvalu"}, {"TW","Taiwán"}, {"UA","Ucrania"},
+        {"UG","Uganda"}, {"US","Estados Unidos de América"}, {"UY","Uruguay"}, {"UZ","Uzbekistán"}, {"VA","Ciudad del Vaticano"}, {"VC","San Vicente y las Granadinas"},
+        {"VE","Venezuela"}, {"VG","Islas Vírgenes Británicas"}, {"VI","Islas Vírgenes de los Estados Unidos de América"}, {"VN","Vietnam"},
+        {"VU","Vanuatu"}, {"WF","Wallis y Futuna"}, {"WS","Samoa"}, {"YE","Yemen"}, {"YT","Mayotte"},
+        {"ZA","Sudáfrica"}
+    };
+    private string ObtenerCodigoPaisOrigen(string nombrePersonaje){
         string url = @$"https://api.nationalize.io?name={nombrePersonaje}";
         var peticion = (HttpWebRequest) WebRequest.Create(url);
         peticion.Method = "GET";
         peticion.ContentType = "application/json";
         peticion.Accept = "application/json";
-        PersonajePaises p = null;
+        PersonajeOrigen p = null;
+        string codPais = String.Empty;
+        int rand;
         
         try
         {
@@ -21,8 +65,15 @@ public class FabricaDePersonajes
                         if (strReader == null) return null;
                         using (StreamReader objReader = new StreamReader(strReader))
                         {
+                            List<string> codPaises = new List<string>(paises.Keys);
+                            bool valido = false;
                             string info = objReader.ReadToEnd();
-                            p = JsonSerializer.Deserialize<PersonajePaises>(info);
+                            p = JsonSerializer.Deserialize<PersonajeOrigen>(info);
+                            while(!valido){
+                                rand = new Random().Next(0, 4);
+                                codPais = p.country[rand].country_id;
+                                if(codPaises.Contains(codPais)) valido = true;
+                            }
                         }
                     }
             }
@@ -32,171 +83,67 @@ public class FabricaDePersonajes
             Console.WriteLine("Problemas de acceso a la API");
         }
 
-        return p;
+        return codPais;
     }
 
-    private string ObtenerNacionalidadPersonaje(PersonajePaises p, string rutaArchivoPaises){
-        string nacionalidadPersonaje = "";
-        int rand;
-        string randString;
-        string linea = "";
-        string[] lineaSplit;
-
-        if(p != null){
-            rand = new Random().Next(0, 5);
-            string codPais = p.country[rand].country_id;
-            using (StreamReader s = new StreamReader(rutaArchivoPaises)){
-                linea = s.ReadLine();
-                while((linea != null) && (nacionalidadPersonaje == "")){
-                    lineaSplit = linea.Split(';');
-                    if(lineaSplit[1] == codPais){
-                        nacionalidadPersonaje = lineaSplit[2];
-                    }
-                    linea = s.ReadLine();
-                }
-            }
-        } else{
-            randString = new Random().Next(0, 240).ToString();
-            using (StreamReader st = new StreamReader(rutaArchivoPaises)){
-                linea = st.ReadLine();
-                while((linea != null) && (nacionalidadPersonaje == "")){
-                    lineaSplit = linea.Split(';');
-                    if(lineaSplit[0] == randString){
-                        nacionalidadPersonaje = lineaSplit[2];
-                    }
-                    linea = st.ReadLine();
-                }
-            }
+    private string ObtenerPaisOrigenPersonaje(string nombrePersonaje){
+        if(ObtenerCodigoPaisOrigen(nombrePersonaje) != null){
+            return paises[ObtenerCodigoPaisOrigen(nombrePersonaje)];
+        }else{
+            List<string> codPaises = new List<string>(paises.Keys);
+            int rnd = new Random().Next(paises.Count);
+            return paises[codPaises[rnd]];
         }
-
-        return nacionalidadPersonaje;
     }
 
     private Personaje GenerarPersonaje(TipoPersonaje tipo){
         Personaje nuevoPersonaje = new Personaje();
-        
+        nuevoPersonaje.Caracteristicas = new CaracteristicasPersonaje();
+        int aux = new Random().Next(0, 3);
+
         switch(tipo){
             case TipoPersonaje.Sicario:
-            nuevoPersonaje = CrearSicario();
+            string[ , ] nombresYApodosSicarios = {{"Dennis", "El oculto"},{"Sara", "La inocente"},{"Robert", "El ingenioso"}};
+            nuevoPersonaje.Nombre = nombresYApodosSicarios[aux, 0];
+            nuevoPersonaje.Apodo = nombresYApodosSicarios[aux, 1];
             break;
             case TipoPersonaje.Psicopata:
-            nuevoPersonaje = CrearPsicopata();
+            string[ , ] nombresYApodosPsicopatas = {{"Jeremy", "El salvaje"},{"Sofia", "La gata"},{"Gaston", "El sangriento"}};
+            nuevoPersonaje.Nombre = nombresYApodosPsicopatas[aux, 0];
+            nuevoPersonaje.Apodo = nombresYApodosPsicopatas[aux, 1];
             break;
             case TipoPersonaje.Ladron:
-            nuevoPersonaje = CrearLadron();
+            string[ , ] nombresYApodosLadrones = {{"Jessica", "La sigilosa"},{"Lucas", "El nocturno"},{"Jorge", "El picaron"}};
+            nuevoPersonaje.Nombre = nombresYApodosLadrones[aux, 0];
+            nuevoPersonaje.Apodo = nombresYApodosLadrones[aux, 1];
             break;
-            case TipoPersonaje.Policia:
-            nuevoPersonaje = CrearPolicia();
+            case TipoPersonaje.Mafioso:
+            string[ , ] nombresYApodosMafiosos = {{"Miguel", "El grosero"},{"Camila", "La gritona"},{"Joshua", "El agresivo"}};
+            nuevoPersonaje.Nombre = nombresYApodosMafiosos[aux, 0];
+            nuevoPersonaje.Apodo = nombresYApodosMafiosos[aux, 1];
             break;
-            case TipoPersonaje.AgenteEspecial:
-            nuevoPersonaje = CrearAgenteEspecial();
+            case TipoPersonaje.Terrorista:
+            string[ , ] nombresYApodosTerroristas = {{"Ricardo", "El mas hábil"},{"Estela", "La actriz"},{"Guillermo", "El guerrero"}};
+            nuevoPersonaje.Nombre = nombresYApodosTerroristas[aux, 0];
+            nuevoPersonaje.Apodo = nombresYApodosTerroristas[aux, 1];
             break;
         }
 
-        nuevoPersonaje.Nacionalidad = ObtenerNacionalidadPersonaje(ObtenerPosiblesPaisesPersonaje(nuevoPersonaje.Nombre), "paises.txt");
+        // Definiendo los datos restantes del personaje en cuestión
+        nuevoPersonaje.Tipo = tipo;
+        nuevoPersonaje.FechaNac = new DateTime(new Random().Next(1980, 2001), new Random().Next(1, 13), new Random().Next(1, 29));
+        nuevoPersonaje.Edad = (DateTime.Now.Subtract(nuevoPersonaje.FechaNac).Days) / 365;
+        nuevoPersonaje.PaisOrigen = ObtenerPaisOrigenPersonaje(nuevoPersonaje.Nombre);
+        
+        // Se generan los valores de las características del personaje en cuestión
+        nuevoPersonaje.Caracteristicas.Nivel = new Random().Next(3, 8);
+        nuevoPersonaje.Caracteristicas.Velocidad = new Random().Next(5, 11);
+        nuevoPersonaje.Caracteristicas.Destreza = new Random().Next(2, 6);
+        nuevoPersonaje.Caracteristicas.Fuerza = new Random().Next(3, 8);
+        nuevoPersonaje.Caracteristicas.Blindaje = new Random().Next(5, 11);
         nuevoPersonaje.Caracteristicas.Salud = 100;
 
         return nuevoPersonaje;
-    }
-
-    private  Personaje CrearSicario(){
-        Personaje sicario = new Personaje();
-        sicario.Caracteristicas = new CaracteristicasPersonaje();
-        int aux = new Random().Next(0, 3);
-
-        string[ , ] nombresYApodos = {{"Dennis", "El oculto"},{"Sara", "La inocente"},{"Robert", "El ingenioso"}};
-        sicario.Nombre = nombresYApodos[aux, 0];
-        sicario.Apodo = nombresYApodos[aux, 1];
-        sicario.Tipo = TipoPersonaje.Sicario;
-        sicario.FechaNac = new DateTime(new Random().Next(1980, 2001), new Random().Next(1, 13), new Random().Next(1, 30)).Date;
-        sicario.Edad = (DateTime.Now.Subtract(sicario.FechaNac).Days) / 365;
-        sicario.Caracteristicas.Nivel = new Random().Next(7, 11);
-        sicario.Caracteristicas.Velocidad = new Random().Next(7, 11);
-        sicario.Caracteristicas.Destreza = new Random().Next(4, 6);
-        sicario.Caracteristicas.Fuerza = new Random().Next(7, 11);
-        sicario.Caracteristicas.Blindaje = new Random().Next(7, 11);
-
-        return sicario;
-    }
-
-    private  Personaje CrearPsicopata(){
-        Personaje psicopata = new Personaje();
-        psicopata.Caracteristicas = new CaracteristicasPersonaje();
-        int aux = new Random().Next(0, 3);
-    
-        string[ , ] nombresYApodos = {{"Jeremy", "El salvaje"},{"Sofia", "La gata"},{"Gaston", "El sangriento"}};
-        psicopata.Nombre = nombresYApodos[aux, 0];
-        psicopata.Apodo = nombresYApodos[aux, 1];
-        psicopata.Tipo = TipoPersonaje.Psicopata;
-        psicopata.FechaNac = new DateTime(new Random().Next(1980, 2001), new Random().Next(1, 13), new Random().Next(1, 30)).Date;
-        psicopata.Edad = (DateTime.Now.Subtract(psicopata.FechaNac).Days) / 365;
-        psicopata.Caracteristicas.Nivel = new Random().Next(2, 4);
-        psicopata.Caracteristicas.Velocidad = new Random().Next(4, 7);
-        psicopata.Caracteristicas.Destreza = new Random().Next(2, 4);
-        psicopata.Caracteristicas.Fuerza = new Random().Next(4, 7);
-        psicopata.Caracteristicas.Blindaje = new Random().Next(4, 7);
-
-        return psicopata;
-    }
-
-    private  Personaje CrearLadron(){
-        Personaje ladron = new Personaje();
-        ladron.Caracteristicas = new CaracteristicasPersonaje();
-        int aux = new Random().Next(0, 3);
-
-        string[ , ] nombresYApodos = {{"Jessica", "La sigilosa"},{"Lucas", "El nocturno"},{"Jorge", "El picaron"}};
-        ladron.Nombre = nombresYApodos[aux, 0];
-        ladron.Apodo = nombresYApodos[aux, 1];
-        ladron.Tipo = TipoPersonaje.Ladron;
-        ladron.FechaNac = new DateTime(new Random().Next(1980, 2000), new Random().Next(1, 13), new Random().Next(1, 29)).Date;
-        ladron.Edad = (DateTime.Now.Subtract(ladron.FechaNac).Days) / 365;
-        ladron.Caracteristicas.Nivel = new Random().Next(1, 3);
-        ladron.Caracteristicas.Velocidad = new Random().Next(1, 4);
-        ladron.Caracteristicas.Destreza = new Random().Next(1, 3);
-        ladron.Caracteristicas.Fuerza = new Random().Next(1, 4);
-        ladron.Caracteristicas.Blindaje = new Random().Next(1, 4);
-
-        return ladron;
-    }
-
-    private  Personaje CrearPolicia(){
-        Personaje policia = new Personaje();
-        policia.Caracteristicas = new CaracteristicasPersonaje();
-        int aux = new Random().Next(0, 3);
-
-        string[ , ] nombresYApodos = {{"Miguel", "El grosero"},{"Camila", "La gritona"},{"Joshua", "El agresivo"}};
-        policia.Nombre = nombresYApodos[aux, 0];
-        policia.Apodo = nombresYApodos[aux, 1];
-        policia.Tipo = TipoPersonaje.Policia;
-        policia.FechaNac = new DateTime(new Random().Next(1980, 2001), new Random().Next(1, 13), new Random().Next(1, 30)).Date;
-        policia.Edad = (DateTime.Now.Subtract(policia.FechaNac).Days) / 365;
-        policia.Caracteristicas.Nivel = new Random().Next(1, 6);
-        policia.Caracteristicas.Velocidad = new Random().Next(1, 6);
-        policia.Caracteristicas.Destreza = new Random().Next(1, 4);
-        policia.Caracteristicas.Fuerza = new Random().Next(1, 6);
-        policia.Caracteristicas.Blindaje = new Random().Next(1, 6);
-
-        return policia;
-    }
-
-    private  Personaje CrearAgenteEspecial(){
-        Personaje agenteEspecial = new Personaje();
-        agenteEspecial.Caracteristicas = new CaracteristicasPersonaje();
-        int aux = new Random().Next(0, 3);
-
-        string[ , ] nombresYApodos = {{"Ricardo", "El mas hábil"},{"Estela", "La actriz"},{"Guillermo", "El guerrero"}};
-        agenteEspecial.Nombre = nombresYApodos[aux, 0];
-        agenteEspecial.Apodo = nombresYApodos[aux, 1];
-        agenteEspecial.Tipo = TipoPersonaje.AgenteEspecial;
-        agenteEspecial.FechaNac = new DateTime(new Random().Next(1980, 2001), new Random().Next(1, 13), new Random().Next(1, 30)).Date;
-        agenteEspecial.Edad = (DateTime.Now.Subtract(agenteEspecial.FechaNac).Days) / 365;
-        agenteEspecial.Caracteristicas.Nivel = new Random().Next(5, 11);
-        agenteEspecial.Caracteristicas.Velocidad = new Random().Next(5, 11);
-        agenteEspecial.Caracteristicas.Destreza = new Random().Next(3, 6);
-        agenteEspecial.Caracteristicas.Fuerza = new Random().Next(5, 11);
-        agenteEspecial.Caracteristicas.Blindaje = new Random().Next(5, 11);
-
-        return agenteEspecial;
     }
 
     private  bool PersonajesIguales(Personaje p1, Personaje p2){
