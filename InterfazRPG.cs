ced0e7b1ae5048e2ff7840f6bde3563e4a17bef9
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-24 00:22:54 -0300

    Actualización y creación de nuevos métodos

--- InterfazRPG.cs
+++ InterfazRPG.cs
@@ -9,91 +9,145 @@ public static class InterfazRPG
         int i = 0;
 
         while(i < 107){
-            Console.Write(@"/");
+            Console.Write(@"▓");
             Thread.Sleep(40);
             i++;
         }
     }
-    public static void MensajesYTituloJuego(int id){
+
+    public static void TituloJuego(){
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("  /$$$$$$  /$$$$$$$  /$$$$$$ /$$      /$$ /$$$$$$ /$$   /$$  /$$$$$$  /$$                   ");
+        Console.WriteLine(" /$$__  $$| $$__  $$|_  $$_/| $$$    /$$$|_  $$_/| $$$ | $$ /$$__  $$| $$                   ");
+        Console.WriteLine("| $$  \\__/| $$  \\ $$  | $$  | $$$$  /$$$$  | $$  | $$$$| $$| $$  \\ $$| $$                   ");
+        Console.WriteLine("| $$      | $$$$$$$/  | $$  | $$ $$/$$ $$  | $$  | $$ $$ $$| $$$$$$$$| $$                   ");
+        Console.WriteLine("| $$      | $$__  $$  | $$  | $$  $$$| $$  | $$  | $$  $$$$| $$__  $$| $$                   ");
+        Console.WriteLine("| $$    $$| $$  \\ $$  | $$  | $$\\  $ | $$  | $$  | $$\\  $$$| $$  | $$| $$                   ");
+        Console.WriteLine("|  $$$$$$/| $$  | $$ /$$$$$$| $$ \\/  | $$ /$$$$$$| $$ \\  $$| $$  | $$| $$$$$$$$            ");
+        Console.WriteLine(" \\______/ |__/  |__/|______/|__/     |__/|______/|__/  \\__/|__/  |__/|________/            ");
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("             /$$$$$$$$ /$$$$$$  /$$$$$$  /$$   /$$ /$$$$$$$$                               ");
+        Console.WriteLine("            | $$_____/|_  $$_/ /$$__  $$| $$  | $$|__  $$__/                               ");
+        Console.WriteLine("            | $$        | $$  | $$  \\__/| $$  | $$   | $$                                  ");
+        Console.WriteLine("            | $$$$$     | $$  | $$ /$$$$| $$$$$$$$   | $$                                  ");
+        Console.WriteLine("            | $$__/     | $$  | $$|_  $$| $$__  $$   | $$                                  ");
+        Console.WriteLine("            | $$        | $$  | $$  \\ $$| $$  | $$   | $$                                  ");
+        Console.WriteLine("            | $$       /$$$$$$|  $$$$$$/| $$  | $$   | $$                                  ");
+        Console.WriteLine("            |__/      |______/ \\______/ |__/  |__/   |__/                                  ");
+        Console.WriteLine("                                                                                           ");
+        Console.WriteLine("                                                                                           ");
+    }
+
+    public static void MostrarPersonajesJuego(List<Personaje> listaP){
+        Thread.Sleep(200);
+        Console.WriteLine("\n==================================== LUCHADORES ================================\n");
+        foreach(var p in listaP){
+            Thread.Sleep(900);
+            p.MostrarDatosYCaracteristicasPersonaje();
+        }
+        Console.Write("\nPresione ENTER para continuar");
+        Console.ReadKey();
+    }
+    public static void MostrarJugadores(List<Jugador> listaJ){
+        Console.WriteLine("\n");
+        Console.WriteLine("   NOMBRE   ║     PERSONAJE ");
+        Console.WriteLine("════════════╬═════════════════");
+        foreach(var j in listaJ){
+            if(j.NombreJugador.Length == 3) Console.WriteLine($"    {j.NombreJugador}     ║  {j.Luchador.Nombre}, {j.Luchador.Apodo}");
+            if(j.NombreJugador.Length == 5) Console.WriteLine($"   {j.NombreJugador}    ║  {j.Luchador.Nombre}, {j.Luchador.Apodo}");
+            if(j.NombreJugador.Length == 6) Console.WriteLine($"  JUG_10    ║  {j.Luchador.Nombre}, {j.Luchador.Apodo}");
+        }
+        Console.Write("\nPresione ENTER para continuar");
+        Console.ReadKey();
+    }
+    public static void Comentarios(int id){
         switch(id){
-            // Título del juego
             case 1:
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("  /$$$$$$  /$$$$$$$  /$$$$$$ /$$      /$$ /$$$$$$ /$$   /$$  /$$$$$$  /$$                   ");
-            Console.WriteLine(" /$$__  $$| $$__  $$|_  $$_/| $$$    /$$$|_  $$_/| $$$ | $$ /$$__  $$| $$                   ");
-            Console.WriteLine("| $$  \\__/| $$  \\ $$  | $$  | $$$$  /$$$$  | $$  | $$$$| $$| $$  \\ $$| $$                   ");
-            Console.WriteLine("| $$      | $$$$$$$/  | $$  | $$ $$/$$ $$  | $$  | $$ $$ $$| $$$$$$$$| $$                   ");
-            Console.WriteLine("| $$      | $$__  $$  | $$  | $$  $$$| $$  | $$  | $$  $$$$| $$__  $$| $$                   ");
-            Console.WriteLine("| $$    $$| $$  \\ $$  | $$  | $$\\  $ | $$  | $$  | $$\\  $$$| $$  | $$| $$                   ");
-            Console.WriteLine("|  $$$$$$/| $$  | $$ /$$$$$$| $$ \\/  | $$ /$$$$$$| $$ \\  $$| $$  | $$| $$$$$$$$            ");
-            Console.WriteLine(" \\______/ |__/  |__/|______/|__/     |__/|______/|__/  \\__/|__/  |__/|________/            ");
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("             /$$$$$$$$ /$$$$$$  /$$$$$$  /$$   /$$ /$$$$$$$$                               ");
-            Console.WriteLine("            | $$_____/|_  $$_/ /$$__  $$| $$  | $$|__  $$__/                               ");
-            Console.WriteLine("            | $$        | $$  | $$  \\__/| $$  | $$   | $$                                  ");
-            Console.WriteLine("            | $$$$$     | $$  | $$ /$$$$| $$$$$$$$   | $$                                  ");
-            Console.WriteLine("            | $$__/     | $$  | $$|_  $$| $$__  $$   | $$                                  ");
-            Console.WriteLine("            | $$        | $$  | $$  \\ $$| $$  | $$   | $$                                  ");
-            Console.WriteLine("            | $$       /$$$$$$|  $$$$$$/| $$  | $$   | $$                                  ");
-            Console.WriteLine("            |__/      |______/ \\______/ |__/  |__/   |__/                                  ");
-            Console.WriteLine("                                                                                           ");
-            Console.WriteLine("                                                                                           ");
+            Console.WriteLine("\n                     ¡QUE BUENA GOLPIZA!                    ");
             break;
 
             case 2:
-            int i = 0, cl = Console.CursorLeft, ct = Console.CursorTop;
-            bool cambiaColor = false;
-
-            while(i < 50){
-                if(!cambiaColor){
-                    Console.ResetColor();
-                    cambiaColor = true;
-                }else{
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    cambiaColor = false;
-                }
-                Console.WriteLine(" .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------. ");
-                Console.WriteLine("| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |");
-                Console.WriteLine("| |    ______    | || |      __      | || | ____  _____  | || |      __      | || |  ________    | || |     ____     | || |  _______     | |");
-                Console.WriteLine("| |  .' ___  |   | || |     /  \\     | || ||_   \\|_   _| | || |     /  \\     | || | |_   ___ `.  | || |   .'    `.   | || | |_   __ \\    | |");
-                Console.WriteLine("| | / .'   \\_|   | || |    / /\\ \\    | || |  |   \\ | |   | || |    / /\\ \\    | || |   | |   `. \\ | || |  /  .--.  \\  | || |   | |__) |   | |");
-                Console.WriteLine("| | | |    ____  | || |   / ____ \\   | || |  | |\\ \\| |   | || |   / ____ \\   | || |   | |    | | | || |  | |    | |  | || |   |  __ /    | |");
-                Console.WriteLine("| | \\ `.___]  _| | || | _/ /    \\ \\_ | || | _| |_\\   |_  | || | _/ /    \\ \\_ | || |  _| |___.' / | || |  \\  `--'  /  | || |  _| |  \\ \\_  | |");
-                Console.WriteLine("| |  `._____.'   | || ||____|  |____|| || ||_____|\\____| | || ||____|  |____|| || | |________.'  | || |   `.____.'   | || | |____| |___| | |");
-                Console.WriteLine("| |              | || |              | || |              | || |              | || |              | || |              | || |              | |");
-                Console.WriteLine("| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |");
-                Console.WriteLine(" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ");
-                Console.SetCursorPosition(cl, ct);
-                Thread.Sleep(600);
-                i++;
-            }
+            Console.WriteLine("\n               ¡NO CREO QUE SANE DESPUÉS DE ESO!           ");
             break;
 
             case 3:
+            Console.WriteLine("\n                    ¡MARAVILLOSA JUGADA!                  ");
+            break;
+
+            case 4:
+            Console.WriteLine("\n        AUCH! POR SUERTE, LA AMBULANCIA SE ENCUENTRA CERCA  ");
+            break;
+
+            case 5:
+            Console.WriteLine("\n                        ¡QUE RAPIDEZ!                        ");
+            break;
+
+            case 6:
+            Console.WriteLine("\n              PARECE QUE HAY UN GANADOR DEL COMBATE          ");
             break;
         }
     }
 
-    public static void MostrarPersonajes(List<Personaje> personajes){
-        foreach (var p in personajes){
-            Console.WriteLine($"┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ PERSONAJE: {p.Nombre.ToUpper()} ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐");
-            Console.WriteLine($"| ===========DATOS===========");
-            Console.WriteLine($"| NOMBRE: {p.Nombre}");
-            Console.WriteLine($"| APODO: {p.Apodo}");
-            Console.WriteLine($"| TIPO DE PERSONAJE: {p.Tipo}");
-            Console.WriteLine($"| FECHA DE NACIMIENTO: {p.FechaNac.ToShortDateString()}");
-            Console.WriteLine($"| NACIONALIDAD: {p.PaisOrigen}");
-            Console.WriteLine($"| EDAD: {p.Edad}");
-            Console.WriteLine($"| ===========CARACTERÍSTICAS===========");
-            Console.WriteLine($"| VELOCIDAD: {p.Caracteristicas.Velocidad}");
-            Console.WriteLine($"| DESTREZA: {p.Caracteristicas.Destreza}");
-            Console.WriteLine($"| FUERZA: {p.Caracteristicas.Fuerza}");
-            Console.WriteLine($"| NIVEL: {p.Caracteristicas.Nivel}");
-            Console.WriteLine($"| BLINDAJE: {p.Caracteristicas.Blindaje}");
-            Console.WriteLine($"| SALUD: {p.Caracteristicas.Salud}");
+    public static void PresentacionCombatientes(Jugador P1, int ataqueP1, int defensaP1, int efectividadP1, Jugador P2, int ataqueP2, int defensaP2, int efectividadP2){
+        Console.WriteLine($"\n════════════════════════════════ {P1.NombreJugador.ToUpper()} ════════════════════════════════\n");
+        Console.WriteLine($"  Personaje: {P1.Luchador.Nombre.ToUpper()}, {P1.Luchador.Apodo.ToUpper()}\n");
+        Console.WriteLine($"  Ataque: {ataqueP1}\n");
+        Console.WriteLine($"  Defensa: {defensaP1}\n");
+        Console.WriteLine($"  Efectividad: {efectividadP1}\n");
+        Console.WriteLine($"  Salud: {P1.Luchador.Caracteristicas.Salud.ToString("0.0")}%\n");
+
+        Console.WriteLine("\n================================ VS ======================================\n");
+        Thread.Sleep(700);
+
+        Console.WriteLine($"\n════════════════════════════════ {P2.NombreJugador.ToUpper()} ════════════════════════════════\n");
+        Console.WriteLine($"  Personaje: {P2.Luchador.Nombre.ToUpper()}, {P2.Luchador.Apodo.ToUpper()}\n");
+        Console.WriteLine($"  Ataque: {ataqueP2}\n");
+        Console.WriteLine($"  Defensa: {defensaP2}\n");
+        Console.WriteLine($"  Efectividad: {efectividadP2}\n");
+        Console.WriteLine($"  Salud: {P2.Luchador.Caracteristicas.Salud.ToString("0.0")}%\n");
+    }
+
+    public static void Ganaste(){
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        string mensaje = @"
+        .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------. 
+        | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+        | |    ______    | || |      __      | || | ____  _____  | || |      __      | || |    _______   | || |  _________   | || |  _________   | |
+        | |  .' ___  |   | || |     /  \     | || ||_   \|_   _| | || |     /  \     | || |   /  ___  |  | || | |  _   _  |  | || | |_   ___  |  | |
+        | | / .'   \_|   | || |    / /\ \    | || |  |   \ | |   | || |    / /\ \    | || |  |  (__ \_|  | || | |_/ | | \_|  | || |   | |_  \_|  | |
+        | | | |    ____  | || |   / ____ \   | || |  | |\ \| |   | || |   / ____ \   | || |   '.___`-.   | || |     | |      | || |   |  _|  _   | |
+        | | \ `.___]  _| | || | _/ /    \ \_ | || | _| |_\   |_  | || | _/ /    \ \_ | || |  |`\____) |  | || |    _| |_     | || |  _| |___/ |  | |
+        | |  `._____.'   | || ||____|  |____|| || ||_____|\____| | || ||____|  |____|| || |  |_______.'  | || |   |_____|    | || | |_________|  | |
+        | |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+        | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+        '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+        ";
+        Console.WriteLine(mensaje);
+        Console.ResetColor();
+    }
+    public static void MensajeGanadorTorneo(Jugador ganador){
+        int altura = 20;
+        Console.WriteLine("\n");
+        if(ganador.NombreJugador.Length == 3) for(int i = 0; i < (altura * 4); i++) Console.Write(" ");
+        if(ganador.NombreJugador.Length == 5) for(int i = 0; i < (altura * 4 - 1); i++) Console.Write(" ");
+        if(ganador.NombreJugador.Length == 6) for(int i = 0; i < (altura * 4 - 2); i++) Console.Write(" ");
+        Console.Write($"{ganador.NombreJugador}\n");
+
+        for(int fila = 1; fila <= altura; fila++){
+            Thread.Sleep(40);
+            for(int espacio = altura * 4 - fila; espacio > 0; espacio--) Console.Write(" ");
+
+            Console.Write("┌");
+
+            for(int linea = 1; linea <= (2*fila + 1); linea++) Console.Write("─");
+
+            Console.Write("┐");
+
+            Console.Write("\n");
         }
+        Ganaste();
     }
 }
