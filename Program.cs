fabc3b4a56d3b93762c2a5027712b61ec6d63060
adanSmith01 <smith.paredes.13@gmail.com>
2023-07-26 00:34:24 -0300

    Actualización del juego

--- Program.cs
+++ Program.cs
@@ -4,67 +4,59 @@ using RPG;
 
 internal class Program
 {
-    
     private static void Main(string[] args){
 
-        // Carga de los datos de los personajes del juego
         string rutaJSON = "rpgPersonajes.json";
         List<Personaje> listaPersonajes = new List<Personaje>();
         var fabricaPersonajes = new FabricaDePersonajes();
         var personajesJSON = new PersonajesJSON();
 
-
         if(!personajesJSON.ExisteArchivoPersonajes(rutaJSON)) personajesJSON.GuardarPersonajes(fabricaPersonajes.GenerarListaPersonajesRPG(), rutaJSON);
-
-        // Carga de los datos del historial de ganadores
-        //string rutaHistorialGanadores = "historial.csv";
-        //Procesos.CargarHistorialGanadores(rutaHistorialGanadores, null);
-
-        // Variables para el ménu principal
+        
+        string rutaHistorialGanadores = "historial.csv";
+        Procesos.PrecargarHistorialGanadores(rutaHistorialGanadores);
+        const int TOP_MEJORES_JUGADORES = 5;
+        
         int opc = 0;
         string eleccion = String.Empty;
 
-        //Lista de jugadores
+        
         List<Jugador> listaJugadores = new List<Jugador>();
 
+        InterfazRPG.MensajeCargando();
+        InterfazRPG.ContextoJuego();
         do{
             InterfazRPG.TituloJuego();
-            
+
             Console.WriteLine("\n                              MENÚ PRINCIPAL                                   ");
             Console.WriteLine("\n                            1. Iniciar juego                                   ");
-            Console.WriteLine("\n                          2. Opciones del juego                                ");
+            Console.WriteLine($"\n                    2. Ver top {TOP_MEJORES_JUGADORES} de mejores jugadores                               ");
             Console.WriteLine("\n                                3. Salir                                       ");
 
-            // Verificar que se ingresa la opción correcta
+            
             do{
                 Console.Write("\nElija la opcion: ");
                 eleccion = Console.ReadLine();
             }while(!int.TryParse(eleccion, out opc) || (opc < 1 || opc > 3));
 
-            // Si opc es 1, se desarrolla el juego
-            if(opc == 1){
-                Jugador ganadorTorneo = new Jugador();
-                listaPersonajes = personajesJSON.LeerPersonajes(rutaJSON);
-                InterfazRPG.MostrarPersonajesJuego(listaPersonajes);
-                listaJugadores = Procesos.CargarJugadores(listaPersonajes);
-                InterfazRPG.MostrarJugadores(listaJugadores);
-                ganadorTorneo = Procesos.TorneoLuchadores(listaJugadores);
-                InterfazRPG.MensajeGanadorTorneo(ganadorTorneo);
-                //Procesos.CargarHistorialGanadores(rutaHistorialGanadores, ganadorTorneo);
-                listaJugadores.Clear();
+            if(opc != 3){
+                if(opc == 1) EjecutarJuego(listaPersonajes, listaJugadores, personajesJSON, rutaHistorialGanadores, rutaJSON);
+                if(opc == 2) InterfazRPG.MostrarTopMejoresJugadores(TOP_MEJORES_JUGADORES, rutaHistorialGanadores);
+                InterfazRPG.MensajeCargando();
             }
-            // Si opc es 2, el usuario puede ver el historial de los ganadores y el top 5 de ganadores
-            // Si opc es 3, no se ejecuta el juego 
+            
         }while((opc != 3));
+    }
 
-        // Borrar datos del juego
-        //BorrarArchivo(rutaHistorialGanadores);
-        //BorrarArchivo(rutaJSON);
-        
+    public static void EjecutarJuego(List<Personaje> listaPersonajes, List<Jugador> listaJugadores, PersonajesJSON cargaPersonajes, string rutaHistorialGanadores, string rutaJSON){
+        Jugador ganadorTorneo = new Jugador();
+        listaPersonajes = cargaPersonajes.LeerPersonajes(rutaJSON);
+        InterfazRPG.MostrarPersonajesJuego(listaPersonajes);
+        listaJugadores = Procesos.CargarJugadores(listaPersonajes);
+        InterfazRPG.MostrarJugadores(listaJugadores);
+        ganadorTorneo = Procesos.TorneoLuchadores(listaJugadores);
+        InterfazRPG.MensajeGanadorTorneo(ganadorTorneo);
+        Procesos.GuardarGanadorAHistorial(rutaHistorialGanadores, ganadorTorneo);
+        listaJugadores.Clear();
     }
-    
-    public static void BorrarArchivo(string ruta){
-        File.Delete(ruta);
-    }
-    
 }
